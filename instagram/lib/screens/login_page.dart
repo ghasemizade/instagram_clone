a49866e8c9361f4ce2b7df2ca7fa194c4b4654e9
Hosein <mrqasemizade@gmail.com>
2022-09-07 18:55:39 +0430

    create login page for project

--- instagram/lib/screens/login_page.dart
+++ instagram/lib/screens/login_page.dart
@@ -29,17 +29,6 @@ class loginPage extends StatelessWidget {
     );
   }
 
-  // decoration: BoxDecoration(
-  //               gradient: LinearGradient(
-  //                 colors: [
-  //                   Color(0xff4E3CC9),
-  //                   Color(0xffF35383),
-  //                 ],
-  //                 begin: Alignment.topCenter,
-  //                 end: Alignment.bottomCenter,
-  //               ),
-  //             ),
-
   Widget _getImageContainer() {
     return Positioned(
       left: 0,
@@ -74,6 +63,108 @@ class loginPage extends StatelessWidget {
               borderRadius: BorderRadius.only(
                 topLeft: Radius.circular(15),
                 topRight: Radius.circular(15),
+              ),
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: double.infinity,
+                  ),
+                  SizedBox(
+                    height: 50.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Sign in to ',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18.0,
+                        ),
+                      ),
+                      Image(
+                        image: AssetImage('assets/Images/mood.png'),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 34,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 44.0,
+                    ),
+                    child: SizedBox(
+                      height: 45.0,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelText: '  Email',
+                          labelStyle: TextStyle(
+                            color: Colors.grey,
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(15),
+                            ),
+                            borderSide: BorderSide(
+                              color: Color(0xffC5C5C5),
+                              width: 3.0,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              width: 3.0,
+                              color: Color(0xffF35383),
+                            ),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(15),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 32,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 44.0,
+                    ),
+                    child: SizedBox(
+                      height: 45.0,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelText: '  Password ',
+                          labelStyle: TextStyle(
+                            color: Colors.grey,
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(15),
+                            ),
+                            borderSide: BorderSide(
+                              color: Color(0xffC5C5C5),
+                              width: 3.0,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              width: 3.0,
+                              color: Color(0xffF35383),
+                            ),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(15),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
               ),
             ),
           ),
