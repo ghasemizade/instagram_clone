ad7559eca86ea11d1ed4ccdccf715788275a7efa
Hosein <mrqasemizade@gmail.com>
2022-09-07 14:55:08 +0430

    create login screen

---
+++ instagram/lib/screens/login_page.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+
+class loginPage extends StatelessWidget {
+  const loginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color(0xff4E3CC9),
+            Color(0xffF35383),
+          ],
+          begin: Alignment.topCenter,
+          end: Alignment.bottomCenter,
+        ),
+      ),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          alignment: AlignmentDirectional.bottomCenter,
+          children: [
+            _getImageContainer(),
+            _getContainerBox(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  // decoration: BoxDecoration(
+  //               gradient: LinearGradient(
+  //                 colors: [
+  //                   Color(0xff4E3CC9),
+  //                   Color(0xffF35383),
+  //                 ],
+  //                 begin: Alignment.topCenter,
+  //                 end: Alignment.bottomCenter,
+  //               ),
+  //             ),
+
+  Widget _getImageContainer() {
+    return Positioned(
+      left: 0,
+      right: 0,
+      bottom: 0,
+      top: 80,
+      child: Column(
+        children: [
+          Expanded(
+            child: Image(
+              image: AssetImage('assets/Images/rocket.png'),
+            ),
+          ),
+          Expanded(
+            child: Container(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _getContainerBox() {
+    return Column(
+      children: [
+        Expanded(
+          child: Container(),
+        ),
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Color(0xff1C1F2E),
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(15),
+                topRight: Radius.circular(15),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
