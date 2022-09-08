299cc3f5a6524e2ac9d824a51d62e3f4f3037f3b
Hosein <mrqasemizade@gmail.com>
2022-09-08 16:35:42 +0430

    update home screen post

--- instagram/lib/screens/home_screen.dart
+++ instagram/lib/screens/home_screen.dart
@@ -38,142 +38,193 @@ class _homePageState extends State<homePage> {
         elevation: 0,
       ),
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Row(
-            children: [
-              // story 1
-              Padding(
-                padding: EdgeInsets.only(left: 27.0, top: 20.0),
-                child: Container(
-                  width: 64,
-                  height: 64,
-                  decoration: BoxDecoration(
-                    color: Color(0xffF35383),
-                    borderRadius: BorderRadius.circular(40),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(3.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Color(0xff1C1F2E),
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: CircleAvatar(
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(30.0),
-                            child: Image(
-                              image:
-                                  AssetImage('assets/Images/HoseinAvatar.jpg'),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
+        child: Row(
+          children: [
+            _getStoryBox(),
+            SizedBox(
+              width: 10.0,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(
+                  height: 20.0,
+                ),
+                Text(
+                  'HoseinGhasemizade',
+                  style: TextStyle(
+                    fontFamily: 'GB',
+                    color: Colors.white,
+                    fontSize: 16.0,
                   ),
                 ),
-              ),
-              // story 2
-              Padding(
-                padding: EdgeInsets.only(left: 27.0, top: 20.0),
-                child: Container(
-                  width: 64,
-                  height: 64,
-                  decoration: BoxDecoration(
-                    color: Color(0xffF35383),
-                    borderRadius: BorderRadius.circular(40),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(3.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Color(0xff1C1F2E),
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: CircleAvatar(
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(30.0),
-                            child: Image(
-                              image: AssetImage('assets/Images/user_1.png'),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
+                SizedBox(
+                  height: 5.0,
+                ),
+                Text(
+                  'frontend developer',
+                  style: TextStyle(
+                    fontFamily: 'GB',
+                    color: Colors.white,
+                    fontSize: 14.0,
                   ),
                 ),
-              ),
-              // story 3
-              Padding(
-                padding: EdgeInsets.only(left: 27.0, top: 20.0),
-                child: Container(
-                  width: 64,
-                  height: 64,
-                  decoration: BoxDecoration(
-                    color: Color(0xffF35383),
-                    borderRadius: BorderRadius.circular(40),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(3.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Color(0xff1C1F2E),
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: CircleAvatar(
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(30.0),
-                            child: Image(
-                              image: AssetImage('assets/Images/user_2.png'),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              // story 4
-              Padding(
-                padding: EdgeInsets.only(left: 27.0, top: 20.0),
-                child: Container(
-                  width: 64,
-                  height: 64,
-                  decoration: BoxDecoration(
-                    color: Color(0xffF35383),
-                    borderRadius: BorderRadius.circular(40),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(3.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Color(0xff1C1F2E),
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: Padding(
-                        padding: const EdgeInsets.all(4.0),
-                        child: CircleAvatar(
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(30.0),
-                            child: Image(
-                              image: AssetImage('assets/Images/user_4.png'),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
+              ],
+            ),
+            Spacer(),
+            Icon(
+              Icons.more_vert,
+              color: Colors.white,
+            ),
+            SizedBox(
+              width: 10.0,
+            ),
+          ],
         ),
       ),
+    );
+  }
+}
+
+class _getStoryBox extends StatelessWidget {
+  const _getStoryBox({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        // story 1
+        Padding(
+          padding: EdgeInsets.only(left: 27.0, top: 20.0),
+          child: Container(
+            width: 64,
+            height: 64,
+            decoration: BoxDecoration(
+              color: Color(0xffF35383),
+              borderRadius: BorderRadius.circular(40),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(3.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color(0xff1C1F2E),
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: CircleAvatar(
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(30.0),
+                      child: Image(
+                        image: AssetImage('assets/Images/HoseinAvatar.jpg'),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+        // story 2
+        // Padding(
+        //   padding: EdgeInsets.only(left: 27.0, top: 20.0),
+        //   child: Container(
+        //     width: 64,
+        //     height: 64,
+        //     decoration: BoxDecoration(
+        //       color: Color(0xffF35383),
+        //       borderRadius: BorderRadius.circular(40),
+        //     ),
+        //     child: Padding(
+        //       padding: const EdgeInsets.all(3.0),
+        //       child: Container(
+        //         decoration: BoxDecoration(
+        //           color: Color(0xff1C1F2E),
+        //           borderRadius: BorderRadius.circular(30),
+        //         ),
+        //         child: Padding(
+        //           padding: const EdgeInsets.all(4.0),
+        //           child: CircleAvatar(
+        //             child: ClipRRect(
+        //               borderRadius: BorderRadius.circular(30.0),
+        //               child: Image(
+        //                 image: AssetImage('assets/Images/user_1.png'),
+        //               ),
+        //             ),
+        //           ),
+        //         ),
+        //       ),
+        //     ),
+        //   ),
+        // ),
+        // // story 3
+        // Padding(
+        //   padding: EdgeInsets.only(left: 27.0, top: 20.0),
+        //   child: Container(
+        //     width: 64,
+        //     height: 64,
+        //     decoration: BoxDecoration(
+        //       color: Color(0xffF35383),
+        //       borderRadius: BorderRadius.circular(40),
+        //     ),
+        //     child: Padding(
+        //       padding: const EdgeInsets.all(3.0),
+        //       child: Container(
+        //         decoration: BoxDecoration(
+        //           color: Color(0xff1C1F2E),
+        //           borderRadius: BorderRadius.circular(30),
+        //         ),
+        //         child: Padding(
+        //           padding: const EdgeInsets.all(4.0),
+        //           child: CircleAvatar(
+        //             child: ClipRRect(
+        //               borderRadius: BorderRadius.circular(30.0),
+        //               child: Image(
+        //                 image: AssetImage('assets/Images/user_2.png'),
+        //               ),
+        //             ),
+        //           ),
+        //         ),
+        //       ),
+        //     ),
+        //   ),
+        // ),
+        // // story 4
+        // Padding(
+        //   padding: EdgeInsets.only(left: 27.0, top: 20.0),
+        //   child: Container(
+        //     width: 64,
+        //     height: 64,
+        //     decoration: BoxDecoration(
+        //       color: Color(0xffF35383),
+        //       borderRadius: BorderRadius.circular(40),
+        //     ),
+        //     child: Padding(
+        //       padding: const EdgeInsets.all(3.0),
+        //       child: Container(
+        //         decoration: BoxDecoration(
+        //           color: Color(0xff1C1F2E),
+        //           borderRadius: BorderRadius.circular(30),
+        //         ),
+        //         child: Padding(
+        //           padding: const EdgeInsets.all(4.0),
+        //           child: CircleAvatar(
+        //             child: ClipRRect(
+        //               borderRadius: BorderRadius.circular(30.0),
+        //               child: Image(
+        //                 image: AssetImage('assets/Images/user_4.png'),
+        //               ),
+        //             ),
+        //           ),
+        //         ),
+        //       ),
+        //     ),
+        //   ),
+        // ),
+      ],
     );
   }
 }
