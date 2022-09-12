fa15692d5b48cc7d714e425209145275f42ccf00
Hosein <mrqasemizade@gmail.com>
2022-09-12 17:13:09 +0430

    update sheet bottom

--- instagram/lib/screens/shareBottomSheet.dart
+++ instagram/lib/screens/shareBottomSheet.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 class shareButtomSheet extends StatelessWidget {
   const shareButtomSheet({this.controller, Key? key}) : super(key: key);
@@ -38,26 +39,105 @@ class shareButtomSheet extends StatelessWidget {
   }
 
   Widget _getContent() {
-    return GridView.builder(
+    // return GridView.builder(
+    //   controller: controller,
+    //   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+    //     crossAxisSpacing: 20,
+    //     mainAxisSpacing: 20,
+    //     crossAxisCount: 4,
+    //   ),
+    //   itemBuilder: ((context, index) {
+    //     return GestureDetector(
+    //       onTap: () {
+    //         print('on tap me');
+    //       },
+    //       child: Container(
+    //         decoration: BoxDecoration(
+    //           border: Border.all(color: Colors.blue),
+    //           borderRadius: BorderRadius.circular(20.0),
+    //         ),
+    //       ),
+    //     );
+    //   }),
+    // );
+
+    return CustomScrollView(
       controller: controller,
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20,
-        crossAxisCount: 4,
-      ),
-      itemBuilder: ((context, index) {
-        return GestureDetector(
-          onTap: () {
-            print('on tap me');
-          },
-          child: Container(
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.blue),
-              borderRadius: BorderRadius.circular(20.0),
-            ),
+      slivers: [
+        SliverToBoxAdapter(
+          child: Column(
+            children: [
+              Container(
+                height: 5,
+                width: 67,
+                margin: EdgeInsets.only(top: 10, bottom: 22),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20.0),
+                  ),
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Share',
+                    style: TextStyle(
+                      fontFamily: 'GB',
+                      fontSize: 20,
+                      color: Colors.white,
+                    ),
+                  ),
+                  Image(
+                    image:
+                        AssetImage('assets/Images/icon_share_bottomsheet.png'),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 32,
+              ),
+              Container(
+                height: 46,
+                width: 340,
+                decoration: BoxDecoration(
+                  color: Color.fromRGBO(255, 255, 255, .4),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(13.0),
+                  ),
+                ),
+                child: Row(
+                  children: [
+                    SizedBox(
+                      width: 15.0,
+                    ),
+                    Icon(
+                      Icons.search,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 32.0,
+              ),
+            ],
           ),
-        );
-      }),
+        ),
+        SliverGrid(
+          delegate: SliverChildBuilderDelegate((context, index) {
+            return Container(
+              color: Colors.blue,
+            );
+          }),
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 4,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+        ),
+      ],
     );
   }
 }
