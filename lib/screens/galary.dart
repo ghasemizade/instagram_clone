4ea13a57e2708848c8f27410970466d42f307820
Hosein <mrqasemizade@gmail.com>
2022-09-19 15:05:41 +0430

    create app bar

--- lib/screens/galary.dart
+++ lib/screens/galary.dart
@@ -10,6 +10,44 @@ class galaryItems extends StatefulWidget {
 class _galaryItemsState extends State<galaryItems> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Row(
+          children: [
+            Text(
+              'Post',
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+            Icon(
+              Icons.keyboard_arrow_down,
+              color: Colors.black,
+              size: 30.0,
+            ),
+            Spacer(),
+            Text(
+              'Next',
+              style: TextStyle(
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+        actions: [
+          Icon(
+            Icons.keyboard_arrow_right,
+            color: Colors.black,
+            size: 30,
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Text('Hosein'),
+      ),
+    );
   }
 }
