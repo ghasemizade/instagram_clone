f28db8d607995ca6716ea470efad31ce1822c83d
Hosein <ghasemizade.hosein@gmail.com>
2023-01-03 21:12:40 +0330

    Update log_screen.dart
    Create TabBar

--- lib/screens/log_screen.dart
+++ lib/screens/log_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class logScreen extends StatefulWidget {
@@ -7,12 +8,43 @@ class logScreen extends StatefulWidget {
   State<logScreen> createState() => _logScreenState();
 }
 
-class _logScreenState extends State<logScreen> {
+class _logScreenState extends State<logScreen>
+    with SingleTickerProviderStateMixin {
+  TabController? _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _tabController = TabController(length: 2, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Hosein'),
+      body: SafeArea(
+        child: Container(
+          color: Color(0xff1C1F2E),
+          height: 55,
+          child: TabBar(
+            labelStyle: TextStyle(
+              fontSize: 16,
+              fontFamily: 'GB',
+            ),
+            indicatorPadding: EdgeInsets.symmetric(horizontal: 18),
+            dragStartBehavior: DragStartBehavior.down,
+            indicatorColor: Color(0xffF35383),
+            controller: _tabController,
+            tabs: [
+              Tab(
+                text: 'Following',
+              ),
+              Tab(
+                text: 'You',
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
